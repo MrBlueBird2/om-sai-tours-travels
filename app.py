2c69859e284fb753c50921414fb8420032f6bbe0
Badri Vishal <bv7887393@gmail.com>
2022-02-15 10:04:08 +0530

    Updated Files

--- app.py
+++ app.py
@@ -9,6 +9,31 @@ import os
 user_name = os.environ.get('USER')
 password = os.environ.get('PASSWORD')
 
+formCode='''
+<div class="row justify-content-center" style="margin-top: -20px;">
+	<div class="col-md-6">
+	<form action="/" method="POST">
+	<div class="mb-3">
+		<label for="phone" class="form-label text-light">Phone Number</label>
+		<input type="text" class="form-control" name="phone" id="phone" aria-describedby="emailHelp"
+		style="background: rgba(255, 255, 255, 0.5)">
+	</div>
+	<div class="mb-3">
+		<label for="pickup" class="form-label text-light">Pickup Location</label>
+		<input type="text" class="form-control" name="pickup" id="pickup"
+		style="background: rgba(255, 255, 255, 0.5)">
+	</div>
+	<div class="mb-3">
+		<label for="name" class="form-label text-light">Your Name</label>
+		<input type="text" class="form-control" name="name" id="name"
+		style="background: rgba(255, 255, 255, 0.5)">
+	</div>
+	<button type="submit" class="btn btn-danger py-2 p-4">Submit</button>
+	</form>
+	</div>
+</div>
+'''
+
 app = Flask(__name__)
 mail = Mail(app)
 
